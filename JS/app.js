f5014a9f0fcf48aa1f2245b5b5841b0bd0456e29
Oweenncito <owenfernando08@gmail.com>
2026-03-02 17:15:17 -0500

    cambios realizados

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+// Archivo de utilidad general para el sitio
+
+console.log('app.js cargado');
+
+// Aquí se pueden añadir funcionalidades comunes a varias páginas,
+// como manejo de carrito, animaciones, cambios en el header, etc.
+
